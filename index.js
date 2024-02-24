ffa62a3d7ba1b3e64138e20397f35623689bd965
“janeth” <janeth.cr.mdz@gmail.com>
2024-02-24 15:48:29 -0500

    iniciar tutorial

---
+++ index.js
@@ -0,0 +1,9 @@
+// msj en consola...console.log("Hola");
+
+
+console.log("mi edad es "+miVariable);
+
+
+console.log("mi edad es "+miVariable);
+let miVariable2;
+const miConstante = 3;//variable q no cambia su valor
